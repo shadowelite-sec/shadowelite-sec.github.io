bc6268600f9d6e2581719fce34738a105050a2d1
shadowelite <alansedp@gmail.com>
2022-03-20 21:58:16 +0530

    null

--- list.py
+++ list.py
@@ -14,14 +14,12 @@ INDEX_TEMPLATE = r"""
 <meta name="description" content="${header}"/>
 
 </head>
-<body>
+<body style="background-color:black;">
     <h2>Index of ${header}</h2>
     <p>
     <table>
         <tbody>
             <tr>
-                <th valign="top"><img src="${ROOTDIR}images/blank.gif"
-                    alt="[ICO]"></th>
                 <th><a href="?C=N;O=D">Name</a></th>
                 <th><a href="?C=M;O=A">Last modified</a></th>
                 <th><a href="?C=S;O=A">Size</a></th>
